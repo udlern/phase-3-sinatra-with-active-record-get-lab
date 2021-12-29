17e32b92fbe4f1cc6f210fc593e25d233b6d5e7c
Noa Udler <udlernoa4@gmail.com>
2021-12-29 13:40:20 -0800

    commit tree e8f9e5943d0882089ed8d7656b1ecc5075ff4bd4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+  get "/bakeries" do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
 
-  # add routes
-
+  get '/bakeries:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json
+  end
 end
